458d92f1da1bbe3a25c3a25b3e417661bd25db7e
Ryan Chou <schou35@gatech.edu>
2022-09-30 20:54:46 -0400

    add spring boot template for spring init

---
+++ Backend/src/test/java/com/gtbackend/gtbackend/GtbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gtbackend.gtbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
