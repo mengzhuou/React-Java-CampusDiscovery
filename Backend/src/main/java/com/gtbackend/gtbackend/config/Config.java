c0647aeab4d948a2ec1bb6481d78972f8d38d705
Ryan Chou <schou35@gatech.edu>
2022-10-15 03:16:01 -0400

    added Password Encoder and Security Filter Chain

--- Backend/src/main/java/com/gtbackend/gtbackend/config/Config.java
+++ Backend/src/main/java/com/gtbackend/gtbackend/config/Config.java
@@ -1,4 +1,25 @@
 package com.gtbackend.gtbackend.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class Config {
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.csrf().disable().authorizeRequests().antMatchers("/api/v1/register**","/api/v1/login", "/api/v1/logout").permitAll()
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll().anyRequest().authenticated()
+                .and()
+                .rememberMe();
+        return http.build();
+    }
 }
