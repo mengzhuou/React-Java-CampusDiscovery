56e893bc3c8bd492ca583201424f5be0b8871c63
Ryan Chou <schou35@gatech.edu>
2022-10-15 03:07:09 -0400

    created User object

--- Backend/src/main/java/com/gtbackend/gtbackend/model/User.java
+++ Backend/src/main/java/com/gtbackend/gtbackend/model/User.java
@@ -1,4 +1,86 @@
 package com.gtbackend.gtbackend.model;
 
-public class User {
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User implements UserDetails {
+    @Id
+    private String email;
+    @NotBlank
+    private String password;
+
+    public User(){
+
+    }
+
+    public User(String email, String password){
+        this.email = email;
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        User tmp;
+        try {
+            tmp = (User) obj;
+        }catch (ClassCastException e){
+            return false;
+        }
+        if(this.email.equals(tmp.getUsername()) &&
+                this.password.equals(tmp.getPassword())){
+            return true;
+        }
+        return false;
+
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(() -> "read");
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
