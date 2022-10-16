dd82cc3ec35743d3da206169d0a0b5a54987d4bb
Sophia Chen <xichen.sophia@gmail.com>
2022-10-16 10:45:26 -0400

    login page

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -7,7 +7,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route }
     from "react-router-dom";
 import First from "./components/SignUpPage";
-import Second from "./components/Second";
+import Second from "./components/LoginPage";
 import Main from "./components/Main";
   
 function App() {
