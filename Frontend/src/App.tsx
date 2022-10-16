b696f1086a41ca691e039485c10cb1598d02c3b0
Ninaou <mo05815@georgiasouthern.edu>
2022-10-16 16:03:26 -0400

    Login page/ formik error validation

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,13 +1,8 @@
-//1. seperate buttons, determine UI for buttons from css or self made
-//2. make css for first and second file
-
-
-import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route }
     from "react-router-dom";
 import First from "./components/SignUpPage";
-import Second from "./components/Second";
+import Second from "./components/LoginPage";
 import Main from "./components/Main";
   
 function App() {
@@ -23,36 +18,4 @@ function App() {
 }
   
 export default App;
-  
-
-
-// const {useState} = React;
-
-// function App() {
-//   const [conter, setConter] = useState(0);
-//   return (
-//     <>
-//       <div className="App">
-//          <span className='heading'>Welcome</span>
-//          <span className='buttonText'>
-//             <Button
-//                 border="dashed"
-//                 color="#c29dff"
-//                 height = "50px"
-//                 onClick={() => console.log("You clicked on the pink circle!")}
-//                 radius = "10%"
-//                 width = "100px"
-//                 children = "Sign Up"
-//               />
-//          </span>
-//          <p>{conter}</p>
-//          <button onClick={() => 
-//             setConter(conter+1)
-//         }>Increase Counter</button>
-         
-//       </div>
-      
-//     </>
-//   );
-// }
 
