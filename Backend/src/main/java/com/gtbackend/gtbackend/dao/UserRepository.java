52ba82ec484cd2d9a7a9f86b4a79f8aa31b0123f
schou35 <schou35@gatech.edu>
2022-10-16 14:59:26 -0400

    deleted old backend

--- Backend/src/main/java/com/gtbackend/gtbackend/dao/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gtbackend.gtbackend.dao;
-
-import com.gtbackend.gtbackend.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, String> {
-}
