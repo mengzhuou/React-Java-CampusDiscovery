4266137cfcd2f0b4a19a8a0853dd3d189d1182af
schou35 <schou35@gatech.edu>
2022-10-29 05:30:00 -0400

    ability to add/remove events and get events by paging

--- Backend/src/main/java/com/gtbackend/gtbackend/dao/EventRepository.java
+++ Backend/src/main/java/com/gtbackend/gtbackend/dao/EventRepository.java
@@ -1,18 +1,20 @@
 package com.gtbackend.gtbackend.dao;
 
 import com.gtbackend.gtbackend.model.Event;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+
 import java.util.List;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, Integer> {
-    @Query(value = "SELECT e FROM Event e WHERE e.id >= ?1 AND e.id <= ?2 ORDER BY e.id")
-    List<Event> findEventByIdRange(Integer start, Integer end);
+    @Query(value = "SELECT e FROM Event e ORDER BY e.id")
+    List<Event> findEventByRange(Pageable pageable);
 
     @Query(value = "SELECT e FROM Event e WHERE e.email = ?1 ORDER BY e.id")
     List<Event> findEventByEmail(String email);
