2b811721b56c45c11c84c1e179a485b403802262
schou35 <schou35@gatech.edu>
2022-11-15 00:10:17 -0500

    bug fix

--- Frontend/src/components/AttendeeBoxForHostManagement.tsx
+++ Frontend/src/components/AttendeeBoxForHostManagement.tsx
@@ -7,22 +7,17 @@ import { hostRemove } from '../helpers/connector'
 class AttendeeBoxForHostManagement extends Component<any,any> {
     constructor(props:any){
         super(props);
-        this.state = {id: this.props.eventNum(), email: this.props.email, status: this.props.status, updateForced: false, ForceUpdateNow: false}
+        this.state = {updateForced: false, ForceUpdateNow: false}
         this.attendeeDel = this.attendeeDel.bind(this);
     }
     componentDidMount(): void {
         this.setState({ForceUpdateNow:true})
     }
-    componentDidUpdate(prevProps: Readonly<any>, prevState: Readonly<any>, snapshot?: any): void {
-        if(this.state.id != this.props.eventNum()){
-            this.setState({id: this.props.eventNum()});  //should probably be id: this.state.id
-        }
-    }
 
     attendeeDel(){
         let conf = window.confirm('Confirm or deny');
         if(conf){
-            hostRemove(this.state.id, this.state.email).then(()=>{ //need to be attendeeId
+            hostRemove(this.props.eventNum(), this.props.email).then(()=>{ //need to be attendeeId
                 alert("successful update");
                 this.props.update();
             }).catch(()=>alert("unsuc update"));
@@ -34,7 +29,7 @@ class AttendeeBoxForHostManagement extends Component<any,any> {
         return (
             <div className='AttendeeBox'>
                 <p>
-                    Email : {this.state.email}  Status : {this.state.status}
+                    Email : {this.props.email}  Status : {this.props.status}
                     {/* delete this attendee */}
                     <button className="eventEditPageButton" onClick={this.attendeeDel}>X</button> 
                 </p>
